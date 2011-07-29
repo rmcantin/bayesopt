c412187500e1e2272198e2e9bd166abe73bc5abc
Ruben Martinez-Cantin <rmcantin@gmail.com>
2011-07-29 02:09:23 +0200

    Debugging python code

--- python/gp.py
+++ python/gp.py
@@ -1,26 +1,26 @@
 import numpy as np
 from math import factorial
-from numpy.matlib import dot,ones,array,eye
+from numpy.matlib import dot,vdot,ones,array,eye
 from scipy.stats import norm
 #norm.pdf(x, loc=0, scale=1) and norm.cdf(x, loc=0, scale=1)
 
 class Kernel:
-    def __init__(self):
-        self.params = {'theta':0.21, 'p':1.6}
+    def __init__(self,params=None):
+        if params == None:
+            self.set_default_params();
+
+    def set_default_params(self):
+        self.params = {'theta':0.21,
+                       'p':1.6}
 
     def mattern(self,x1,x2):
-        theta = self.params['theta']
-        xdiff = x1 - x2
+        theta = self.params['theta']   # sqrt(3) / l
+        r = abs(x1 - x2)
+        dif = r / theta
+        acsum = np.sum(dif)
+        acprod = np.prod(1+dif)
 
-        acsum = 0
-        acprod = 1
-
-        for xd in xdiff:
-            dif = abs(xd) / theta
-            acsum += dif
-            acprod *= 1+dif
-
-        return acprod*exp(-acsum)
+        return acprod*np.exp(-acsum)
 
     
 class GaussianProcess:
@@ -35,7 +35,7 @@ class GaussianProcess:
         self.prior_beta = 0.1
         self.prior_delta = 10.0
 
-        self.gp_noise = 1e-4
+        self.obs_noise = 1e-4
         self.k = Kernel()
     
     def correlationFunction(self,x1,x2):
@@ -71,23 +71,23 @@ class GaussianProcess:
         return dot(dot(m,invC),m)
 
     def inverseCorrelation(self):
-        R = eye(self.y.shape) * self.gp_noise;
+        R = eye(self.y.shape[0]) * self.obs_noise;
         for i,xa in enumerate(self.x):
             for j,xb in enumerate(self.x):
-                R[i,j] += correlationFunction(xa,xb)
+                R[i,j] += self.correlationFunction(xa,xb)
 
         return np.linalg.inv(R)
                 
 
     def precomputeGPparams(self,invR):
-        N = self.y.shape
-        uno = ones(N,1)  #TODO: Generalize for other mean functions
+        N = self.y.shape[0]
+        uno = ones(N)  #TODO: Generalize for other mean functions
         alpha = self.prior_alpha
         beta  = self.prior_beta
         delta = self.prior_delta
 
         uInvR = dot(uno,invR)
-        eta = dot(uInvR,uno) + 1/delta
+        eta = vdot(uInvR,uno) + 1/delta
         yRy = mahalanobisDistance(self.ny,invR)
         
         mu = dot(uInvR,self.ny) / eta;
@@ -97,14 +97,15 @@ class GaussianProcess:
 
 
     def prediction(self,query,mu,sig2,uInvR,eta,invR):
-        uno = ones(N,1)  #TODO: Generalize for other mean functions
+        N = self.y.shape[0]
+        uno = ones(N)  #TODO: Generalize for other mean functions
         r = array([])
         for x in self.x:
-            r = np.r_[r,correlationFunction(x,query,theta)]
+            r = np.r_[r,correlationFunction(x,query)]
 
         rInvR = dot(r,invR)
-        uInvRr = dot(uInvR,r)
-        rInvRr = dot(rInvR,r)
+        uInvRr = vdot(uInvR,r)
+        rInvRr = vdot(rInvR,r)
 
         ypred  = mu + rInvR * (self.ny - uno * mu)
         spred2 = sig2 + (1 - rInvRr + (1 - uInvRr)**2 / eta )
@@ -112,11 +113,11 @@ class GaussianProcess:
         return ypred,spred2
 
     def plotResults(self):
-        invR = inverseCorrelation();
-        mu, sig2, uInvR, eta = precomputeGPparams(invR)
+        invR = self.inverseCorrelation();
+        mu, sig2, uInvR, eta = self.precomputeGPparams(invR)
         xl = range(0,1000) / 1000
         for x in xl:
-            y,s = prediction(x,mu,sig2,uInvR,eta,invR)
+            y,s = self.prediction(x,mu,sig2,uInvR,eta,invR)
             plot(x,y,'k')
             plot(x,y+s,'r')
             plot(x,y-s,'r')
@@ -128,7 +129,7 @@ class GaussianProcess:
 
 
     def run(self):
-        for i in range(1,10):
+        for i in range(1,3):
             x = float(raw_input('Insert x:'))
             y = float(raw_input('Insert y:'))
             self.addNewPoint(x,y)

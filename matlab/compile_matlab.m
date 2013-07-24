3a94e691a4d621c0e2ceadcea6574ab0470f4e8c
Ruben Martinez-Cantin <rmcantin@unizar.es>
2013-07-24 02:20:26 +0200

    Simplifying linking among interface. Now python module is build separately.

--- matlab/compile_matlab.m
+++ matlab/compile_matlab.m
@@ -20,24 +20,30 @@ if (ispc)
             -I../include -I../wrappers
     end
 else
-    if exist('../lib/libbayesopt.a','file')
-        disp('Compiling static library');
-        mex -output bayesoptcont bayesoptmex.c ../lib/libbayesopt.a ...
-        ../lib/libnlopt.a -I../include -I../wrappers -I../nlopt/api 
+    mex -output bayesoptcont bayesoptmex.c -L../lib -lbayesopt ...
+        -lnlopt -I../include -I../wrappers -I../nlopt/api 
 
-        mex -output bayesoptdisc bayesoptdiscmex.c ../lib/libbayesopt.a ...
-            ../lib/libnlopt.a -I../include -I../wrappers -I../nlopt/api 
-    else
-        if exist('../lib/bayesopt.so','file')
-            disp('Compiling dynamic library');
-            mex -g -output bayesoptcont bayesoptmex.c ../lib/bayesopt.so ...
-                -I../include -I../wrappers
+    mex -output bayesoptdisc bayesoptdiscmex.c -L../lib -lbayesopt ...
+        -lnlopt -I../include -I../wrappers -I../nlopt/api 
 
-            mex -g -output bayesoptdisc bayesoptdiscmex.c ../lib/bayesopt.so ...
-                -I../include -I../wrappers
+    % if exist('../lib/libbayesopt.a','file')
+    %     disp('Compiling static library');
+    %     mex -output bayesoptcont bayesoptmex.c ../lib/libbayesopt.a ...
+    %     ../lib/libnlopt.a -I../include -I../wrappers -I../nlopt/api 
+
+    %     mex -output bayesoptdisc bayesoptdiscmex.c ../lib/libbayesopt.a ...
+    %         ../lib/libnlopt.a -I../include -I../wrappers -I../nlopt/api 
+    % else
+    %     if exist('../lib/bayesopt.so','file')
+    %         disp('Compiling dynamic library');
+    %         mex -g -output bayesoptcont bayesoptmex.c ../lib/bayesopt.so ...
+    %             -I../include -I../wrappers
+
+    %         mex -g -output bayesoptdisc bayesoptdiscmex.c ../lib/bayesopt.so ...
+    %             -I../include -I../wrappers
                 
-        else
-            disp('Error: File not found');
-        end
-    end
+    %     else
+    %         disp('Error: File not found');
+    %     end
+    % end
 end

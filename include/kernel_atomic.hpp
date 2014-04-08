1c51cbe7700754b7317208954851db73e9e5d484
Ruben Martinez-Cantin <rmcantin@unizar.es>
2014-04-08 23:37:46 +0200

    Adding hamming distance kernel for categorical data.

--- include/kernel_atomic.hpp
+++ include/kernel_atomic.hpp
@@ -131,6 +131,51 @@ namespace bayesopt
     { assert(false); return 0.0;  };
   };
 
+  /** Kernel for categorical data. It measures the hamming distance
+   *  between vectors. 
+   */
+  class HammingKernel: public AtomicKernel
+  {
+  public:
+    void init(size_t input_dim)
+    { n_params = 1; n_inputs = input_dim;  };
+
+    size_t hammingDistance(const vectori& s1, const vectori& s2)
+    {
+      size_t hdist = 0;
+      vectori::const_iterator i1;
+      vectori::const_iterator i2;
+      for( i1 = s1.begin(), i2 = s2.begin(); 
+	   i1 < s1.end() && i2 < s2.end();
+	   ++i1, ++i2 )
+	{
+	  hdist += (*i1 == *i2) ? 0 : 1;
+	}
+      return hdist;
+    }
+
+    double operator()(const vectord &x1, const vectord &x2)
+    { 
+      const size_t n = x1.size();
+      const double ll = params(0);
+      vectori s1(n);
+      vectori s2(n);
+
+      for(size_t i=0; i<n; ++i)
+	{
+	  // We add 0.5 to avoid floating point approximation errors
+	  s1(i) = static_cast<int>(x1(i)+0.5);
+	  s2(i) = static_cast<int>(x2(i)+0.5);
+	}
+
+      return std::exp(-ll/2*static_cast<double>(hammingDistance(s1,s2)));
+    };
+
+    double gradient(const vectord &x1, const vectord &x2,
+		    size_t component)
+    { return 0.0; };
+  };
+
 
   ///////////////////////////////////////////////////////////////////////////
   /** \brief Abstract class for isotropic kernel functors */
